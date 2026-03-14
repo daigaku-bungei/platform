9033c3a81bcc6a49ba669be72f6a6f2f6a1564e4
daigaku-bungei <kotaushi170528@gmail.com>
2026-03-14 21:25:47 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,10 @@ def create_app():
 
     # 設定
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-12345')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite://')
+    db_url = os.environ.get('DATABASE_URL', 'sqlite://')
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
