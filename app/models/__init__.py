1d870bcba084d5be5982f449c0111ec083b7a0eb
daigaku-bungei <kotaushi170528@gmail.com>
2026-04-13 10:02:14 +0900

    Add Character and Plot to models export

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models.user import User
 from app.models.novel import Novel
 from app.models.comment import Comment
 from app.models.like import Like
+from app.models.studio import Character, Plot
 
 
 __all__ = ['User', 'Novel', 'Comment', 'Like']
